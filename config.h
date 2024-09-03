379c87d6678aeb455d7578fb7343658ae78a30b6
p0manm2003 <146773057+p0manm2003@users.noreply.github.com>
2024-09-03 15:00:56 +0300

    Add files via upload

---
+++ config.h
@@ -0,0 +1,19 @@
+/**
+ * Copyright 2012-2013(c) Analog Devices, Inc.
+ *
+ * Licensed under the GPL-2.
+ *
+ **/
+
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+# define OSC_GLADE_FILE_PATH "C:/Users/Роман/Desktop/osc/iio-oscilloscope-main-master/out/install/x64-Debug/share/osc/glade/"
+# define OSC_PLUGIN_PATH "C:/Users/Роман/Desktop/osc/iio-oscilloscope-main-master/out/install/x64-Debug/lib/osc/"
+# define OSC_XML_PATH "C:/Users/Роман/Desktop/osc/iio-oscilloscope-main-master/out/install/x64-Debug/lib/osc/xmls"
+# define OSC_FILTER_FILE_PATH "C:/Users/Роман/Desktop/osc/iio-oscilloscope-main-master/out/install/x64-Debug/lib/osc/filters"
+# define OSC_WAVEFORM_FILE_PATH "C:/Users/Роман/Desktop/osc/iio-oscilloscope-main-master/out/install/x64-Debug/lib/osc/waveforms"
+# define OSC_PROFILES_FILE_PATH "C:/Users/Роман/Desktop/osc/iio-oscilloscope-main-master/out/install/x64-Debug/lib/osc/profiles"
+# define OSC_STYLE_FILE_PATH "C:/Users/Роман/Desktop/osc/iio-oscilloscope-main-master/out/install/x64-Debug/share/osc/"
+
+#endif
